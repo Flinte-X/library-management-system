7fab182b66fc9219569628fe9400409659415869
Suchira Liyanage <suchiraliyanage94@gmail.com>
2020-12-23 22:06:23 +0530

    Finished navbar-setting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,4 +18,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [UserLoginComponent, UserSignupComponent, AdminLoginComponent];
+export const routingComponents = [UserLoginComponent, UserSignupComponent, AdminLoginComponent, PageNotFoundComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {AppRoutingModule, routingComponents} from './app-routing.module';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
-    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
