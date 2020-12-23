2d1d727605aa52e4f53fe84f41e414a22913c8c6
Suchira Liyanage <suchiraliyanage94@gmail.com>
2020-12-23 23:05:58 +0530

    Setting admin-login and admin-dashboard routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import {UserSignupComponent} from './user-signup/user-signup.component';
 import {AdminLoginComponent} from './admin-login/admin-login.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {UserDashbaordComponent} from './user-dashbaord/user-dashbaord.component';
+import {AdminDashbaordComponent} from './admin-dashbaord/admin-dashbaord.component';
 
 const routes: Routes = [
   { path: 'user-login', component: UserLoginComponent},
   { path: 'user-signup', component: UserSignupComponent},
   { path: 'admin-login', component: AdminLoginComponent},
   { path: 'user-dashboard', component: UserDashbaordComponent},
+  { path: 'admin-dashboard', component: AdminDashbaordComponent},
   { path: '', redirectTo: '/admin-login', pathMatch: 'full'},
   { path: '**', component: PageNotFoundComponent}
 ];
